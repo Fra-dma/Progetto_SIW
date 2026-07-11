4af6ec87e497f95404b52f7f58675cabbebd9f36
Fringiskius <giginski875@gmail.com>
2026-07-11 17:15:52 +0100

    Primo commit setup

---
+++ src/main/java/it/uniroma3/siw/SiwProgettoPartite1Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoPartite1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoPartite1Application.class, args);
+	}
+
+}
